9b6f3d21019fc6342cb1a5d8442fc5b8b1a20fbb
Kyle Hochdoerfer <kjhoch2@gmail.com>
2024-02-11 15:12:03 -0500

    Added mongo file and config

---
+++ server/utils/config.js
@@ -0,0 +1,22 @@
+///--------------------------------------------
+//Title: config.js
+//Author: Kyle Hochdoerfer
+//Date: 02/11/24
+//Description: Configuration file for BCRS
+//---------------------------------------------
+
+"use strict"
+
+const db = {
+  username: "bcrs_user",
+  password: "s3cret",
+  name: "bcrsDB"
+}
+
+const config = {
+  port: 3000,
+  dbUrl: `mongodb+srv://${db.username}:${db.password}@cluster0.tydee4p.mongodb.net/nodebucket?retryWrites=true&w=majority`,
+  dbname: db.name
+}
+
+module.exports = config;
