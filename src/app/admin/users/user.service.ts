34bac7034e5fe2de97ac0a265a04dfbab1373b45
Kyle Hochdoerfer <kjhoch2@gmail.com>
2024-02-17 12:36:41 -0500

    Deletion logic added

--- src/app/admin/users/user.service.ts
+++ src/app/admin/users/user.service.ts
@@ -18,26 +18,30 @@ export class UserService {
 
   constructor(private http: HttpClient ) { }
 
-   /**
-  * @description update function to update a information for an empId
-  * @param empId
-  * @returns status code 204 (no content)
-  */
- updateUser(empId: number, user: UserUpdateModel) {
-  console.log('api/users/' + empId + '/update');
-  // update selected user's information based on empId
-  return this.http.put('api/users/' + empId, { user })
- }
+    /**
+    * @description update function to update a information for an empId
+    * @param empId
+    * @returns status code 204 (no content)
+    */
+    updateUser(empId: number, user: UserUpdateModel) {
+    console.log('api/users/' + empId + '/update');
+    // update selected user's information based on empId
+    return this.http.put('api/users/' + empId, { user })
+  }
 
- //Function that returns the URL API for getting all users from the database
- getUsers(){
-  return this.http.get('/api/users');
-}
+  //Function that returns the URL API for getting all users from the database
+  getUsers(){
+    return this.http.get('/api/users');
+  }
 
-// Function that returns User by empId
-getUser(empId: number) {
-  return this.http.get('/api/users/' + empId)
+  // Function that returns User by empId
+  getUser(empId: number) {
+    return this.http.get('/api/users/' + empId)
 
-}
+  }
 
+  //Function that returns the url for user deletion
+  deleteUser(empId: string) {
+    return this.http.delete(`/api/users/${empId}`);
+  }
 }
